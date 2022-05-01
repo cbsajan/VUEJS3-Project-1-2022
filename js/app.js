b1545eac1123d6bf9bd514bde2e42eba2c98aa14
C B Sajan <sajan@ckhathri.com>
2022-05-01 17:41:24 +0530

    Final Commit

--- js/app.js
+++ js/app.js
@@ -1,9 +1,20 @@
+
+
+
 const app = Vue.createApp({
     data(){
         return {
             state:true,
             inputName:'',
-            names:[]
+            names:['Lisa','Ron','Steve'],
+            error:'',
+            showError:false,
+            result:''
+        }
+    },
+    computed:{
+        isReady(){
+            return this.names.length > 1;
         }
     },
     methods:{
@@ -12,17 +23,56 @@ const app = Vue.createApp({
             if(this.validate(userName)){
                 this.names.push(userName);
                 this.inputName = '';
-                console.log(this.names)
-
+                this.showError = false;
             } else {
-                console.log('error')
+                this.showError = true;
             }
         },
         validate(value){
-            /////
+            this.error = '';
+            if(value === '' ){
+                this.error = 'Sorry, no empty name';
+                return false;
+            }
+
+            if(this.names.includes(value)){
+                this.error = 'Sorry, names must be unique';
+                return false;
+            }
             return true
+        },
+        removeName(index){
+            this.names.splice(index,1)
+        },
+        getRandomName(){
+            return this.names[Math.floor(Math.random() * this.names.length)]
+        },
+        generateResult(){
+            let rand = this.getRandomName();//lisa
+
+            if(this.result !== ''){
+                while (rand === this.result){
+                    rand = this.getRandomName();
+                }
+            }
+            this.result = rand;
+        },
+        showResults(){
+            this.generateResult();
+            this.state = false;
+        },
+        resetApp(){
+            this.state = true;
+            this.inputName ='';
+            this.names = [];
+            this.error = '';
+            this.showError = false;
+            this.result = '';
+        },
+        getNewResult(){
+            this.generateResult();
         }
     }
 
 
-}).mount('#app');
+}).mount('#app'); 
